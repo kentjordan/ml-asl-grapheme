a105329ae36187cdf0fb3c8e75e2bd2c87a984c4
Kent Jordan <kentaki.dev@gmail.com>
2024-05-22 16:10:49 +0800

    refactor: used explicitly classes than dataloader

--- api.py
+++ api.py
@@ -7,10 +7,9 @@ import io
 import torch
 from torchvision.transforms import Resize, ToTensor, Compose
 
-from datasets import ASLTrainDataset
 from model import ASLModel
+from classes import classes
 
-train_dataset = ASLTrainDataset("./dataset/asl_alphabet_train")
 model = ASLModel()
 model.load_state_dict(torch.load("./models/dev.pt"))
 
@@ -18,17 +17,18 @@ app = FastAPI()
 
 @app.post('/predict/{w}/{h}')
 async def predict(photo: UploadFile, w: int = 200, h: int = 200):
-
-    image = Image.open(io.BytesIO(await photo.read()))
     
+    image = Image.open(io.BytesIO(await photo.read()))
+
     transforms = Compose([
         Resize(size=(w, h)),
         ToTensor()
     ])
+
     image = transforms(image)
     image = torch.unsqueeze(image, 0) # type: ignore
     output = model(image)
-    prediction = train_dataset.classes[torch.argmax(output)]
+    prediction = classes[torch.argmax(output)]
 
     return {
         "prediction": prediction
